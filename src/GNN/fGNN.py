a4a9a78a758530dd202f1df095d11e7f0d584e5f
Ata Shaker <ata.shaker@tum.de>
2025-12-28 00:49:32 +0100

    Add Edge variant to fGNN

--- src/GNN/fGNN.py
+++ src/GNN/fGNN.py
@@ -1,11 +1,10 @@
-from torch_geometric.loader import NeighborLoader
-
 from src import *
+from src.classifier import Classifier, EdgeClassifier
 from src.utils.graph import Graph
-from src.classifier import Classifier
+from torch_geometric.loader import NeighborLoader
 from src.models.model_binders import (
-    ModelBinder,
     ModelSpecs,
+    ModelBinder,
 )
 
 
@@ -42,4 +41,62 @@ class FGNN(Classifier):
 
     def get_embeddings(self):
         H = self.model(self.graph.x, self.graph.edge_index)
+        return H
+
+
+class FEdgeGNN(EdgeClassifier):
+    def __init__(
+        self, graph: Graph, link_feature_operator: LinkFeatureOperator = "hadamard"
+    ):
+        super().__init__(graph, link_feature_operator)
+        self.create_smodel()
+
+    def create_smodel(self):
+        gnn_layer_sizes = [
+            self.graph.num_features
+        ] + config.feature_model.gnn_layer_sizes
+
+        model_specs = [
+            ModelSpecs(
+                type="GNN",
+                layer_sizes=gnn_layer_sizes,
+                final_activation_function="linear",
+                # final_activation_function="relu",
+                normalization="layer",
+                # normalization="batch",
+            )
+        ]
+
+        self.model: ModelBinder = ModelBinder(model_specs)
+        self.model.to(device)
+
+        match self.link_feature_operator:
+            case "hadamard" | "concat":
+                if self.link_feature_operator == "hadamard":
+                    logistic_regression_layer_sizes = [gnn_layer_sizes[-1], 1]
+                else:
+                    logistic_regression_layer_sizes = [2 * gnn_layer_sizes[-1], 1]
+
+                logistic_regression_model_specs = [
+                    ModelSpecs(
+                        type="MLP",
+                        dropout=0.0,
+                        layer_sizes=logistic_regression_layer_sizes,
+                        final_activation_function=None,  # pyright: ignore
+                        normalization=None,
+                    )
+                ]
+                self.logistic_regression_model = ModelBinder(
+                    logistic_regression_model_specs
+                )
+                self.logistic_regression_model.to(device)
+            case "dot-product":
+                self.logistic_regression_model = None
+            case _:
+                raise NotImplementedError(
+                    f"Operator {self.link_feature_operator} not implemented!"
+                )
+
+    def get_embeddings(self):
+        H = self.model(self.graph.x, self.graph.message_passing_edge_index)
         return H
