9b2399de87f8ed4309ef802c83a00e524c0b7a91
Ata Shaker <ata.shaker@tum.de>
2025-12-28 00:48:07 +0100

    Refactor SClassifier with a mixin class and add SEdgeClassifier

--- src/GNN/sGNN.py
+++ src/GNN/sGNN.py
@@ -1,23 +1,20 @@
 import torch
-from torch_geometric.loader import NeighborLoader
-
 from src import *
+from src.classifier import Classifier, EdgeClassifier
 from src.utils.graph import Graph
-from src.classifier import Classifier
+from torch_geometric.loader import NeighborLoader
 from src.models.model_binders import (
-    ModelBinder,
     ModelSpecs,
+    ModelBinder,
 )
 
 
-class SClassifier(Classifier):
-    def __init__(
-        self,
-        graph: Graph,
-        hidden_layer_size=config.structure_model.DGCN_structure_layers_sizes,
-    ):
-        super().__init__(graph)
-        self.create_smodel(hidden_layer_size)
+class SFVMixin:
+    """
+    Mixin class that provides Structure Feature Vector (SFV) functionality.
+    This mixin adds support for treating graph.x as a trainable parameter.
+    Can be used with both Classifier and EdgeClassifier base classes.
+    """
 
     def parameters(self):
         parameters = super().parameters()
@@ -44,6 +41,22 @@ class SClassifier(Classifier):
         if self.graph.x.requires_grad:
             self.graph.x.grad = None
 
+    def get_embeddings(self):
+        H = self.model(self.graph.x)
+        nodes = self.graph.node_ids
+        H = H[nodes]
+        return H
+
+
+class SClassifier(SFVMixin, Classifier):
+    def __init__(
+        self,
+        graph: Graph,
+        hidden_layer_size=config.structure_model.DGCN_structure_layers_sizes,
+    ):
+        super().__init__(graph)
+        self.create_smodel(hidden_layer_size)
+
     def create_smodel(self, hidden_layer_size=[]):
         layer_sizes = (
             [self.graph.num_features] + hidden_layer_size + [self.graph.num_classes]
@@ -61,11 +74,61 @@ class SClassifier(Classifier):
         self.model: ModelBinder = ModelBinder(model_specs)
         self.model.to(device)
 
-    def get_embeddings(self):
-        H = self.model(self.graph.x)
-        nodes = self.graph.node_ids
-        H = H[nodes]
-        return H
+
+class SEdgeClassifier(SFVMixin, EdgeClassifier):
+    def __init__(
+        self,
+        graph: Graph,
+        link_feature_operator: LinkFeatureOperator = "hadamard",
+        hidden_layer_size=config.structure_model.DGCN_structure_layers_sizes,
+    ):
+        super().__init__(graph, link_feature_operator)
+        self.link_feature_operator = link_feature_operator
+        self.create_smodel(hidden_layer_size)
+
+    def create_smodel(self, hidden_layer_size=[]):
+        layer_sizes = (
+            [self.graph.num_features] + hidden_layer_size 
+        )
+
+        model_specs = [
+            ModelSpecs(
+                type="MLP",
+                layer_sizes=layer_sizes,
+                final_activation_function="linear",
+                normalization="layer",
+            ),
+        ]
+
+        self.model: ModelBinder = ModelBinder(model_specs)
+        self.model.to(device)
+
+        match self.link_feature_operator:
+            case "hadamard" | "concat":
+                if self.link_feature_operator == "hadamard":
+                    logistic_regression_layer_sizes = [layer_sizes[-1], 1]
+                else:
+                    logistic_regression_layer_sizes = [2 * layer_sizes[-1], 1]
+
+                logistic_regression_model_specs = [
+                    ModelSpecs(
+                        type="MLP",
+                        dropout=0.0,
+                        layer_sizes=logistic_regression_layer_sizes,
+                        final_activation_function=None,  # pyright: ignore
+                        normalization=None,
+                    )
+                ]
+                self.logistic_regression_model = ModelBinder(
+                    logistic_regression_model_specs
+                )
+                self.logistic_regression_model.to(device)
+            case "dot-product":
+                self.logistic_regression_model = None
+            case _:
+                raise NotImplementedError(
+                    f"Operator {self.link_feature_operator} not implemented!"
+                )
 
 
 class SGNNSlave(Classifier):
